7193815f828a8bd23107676ebff96e7b63b0ac33
NatiGK <natnaelgashu2022@gmail.com>
2023-10-23 12:49:41 +0300

    add background

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
 class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
   late final CameraComponent cam;
+  @override
   final world = Level();
   @override
   FutureOr<void> onLoad() {
